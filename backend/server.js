3ad9a6357ef9da3533f8fa23b5d357f2599863fc
nahid071 <mdnahid071@gmail.com>
2022-09-07 23:50:40 +0600

    done

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const path = require("path")
 const dotenv = require("dotenv").config()
 const colors = require("colors")
 const connectDB = require("./config/db")
@@ -16,6 +17,19 @@ app.use(express.urlencoded({ extended: false }))
 app.use("/api/goals", require("./routes/goalRoutes"))
 app.use("/api/members", require("./routes/memberRoutes"))
 app.use("/api/users", require("./routes/userRoutes"))
+
+// serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")))
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  )
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"))
+}
 
 app.use(errorHandler)
 
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import { useSelector, useDispatch } from "react-redux"
 import GoalForm from "../components/goalForm"
+import GoalItem from "../components/GoalItem"
 import Spinner from "../components/Spinner"
 import { getGoals, reset } from "../features/goals/goalSlice"
 
@@ -43,6 +44,18 @@ const Dashboard = () => {
       </section>
 
       <GoalForm />
+
+      <section className="content">
+        {goals.length > 0 ? (
+          <div className="goals">
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
+          </div>
+        ) : (
+          <h3>No goal added yet</h3>
+        )}
+      </section>
     </>
   )
 }
